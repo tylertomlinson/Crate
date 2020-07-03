2cd1065783be67fadb74bb86cad79a3610b4b58a
“Krista <stadler.krista@gmail..com>
2020-07-03 14:19:01 -0600

    Annotated index.js and queries.js from schema folder

--- code/api/src/setup/schema/index.js
+++ code/api/src/setup/schema/index.js
@@ -1,12 +1,16 @@
+//file where GraphQL API structure starts
+
 // Imports
 import { GraphQLSchema } from 'graphql'
 
 // App Imports
+//locations of files needed for schema
 import query from './queries'
 import mutation from './mutations'
 
 // Schema
 const schema = new GraphQLSchema({
+  //implementation of variables imported above
   query,
   mutation
 })
--- code/api/src/setup/schema/queries.js
+++ code/api/src/setup/schema/queries.js
@@ -2,12 +2,14 @@
 import { GraphQLObjectType } from 'graphql'
 
 // App Imports
+//imports root queries for each model and assigns them as variables
 import * as user from '../../modules/user/query'
 import * as product from '../../modules/product/query'
 import * as crate from '../../modules/crate/query'
 import * as subscription from '../../modules/subscription/query'
 
 // Query
+//compiles queries for all of the models and assigns all model fields as part of the query template
 const query = new GraphQLObjectType({
   name: 'query',
   description: 'API Queries [Read]',
