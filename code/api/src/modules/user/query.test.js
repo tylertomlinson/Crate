cff88df0add780709fa96f746741444b3d63d7a3
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-07-09 11:32:15 -0600

    Add test for user gender

--- code/api/src/modules/user/query.test.js
+++ code/api/src/modules/user/query.test.js
@@ -28,4 +28,13 @@ describe ("user queries", () => {
 
     expect(response.body.data.users.length).toEqual(3)
   })
+
+  it("returns users gender", async() => {
+    const response= await request(server)
+    .get('/')
+    .send({ query: '{ users { gender } }'})
+    .expect(200)
+
+    expect(response.body.data.users[2]).toEqual({"gender": 1})
+  })
 })
