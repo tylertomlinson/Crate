4bc56de59d1995c251dba3bd5d9f55696b01e7f6
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-07-11 13:48:45 -0600

    Move DB seeds to mock folder

--- code/api/src/mock/index.js
+++ code/api/src/mock/index.js
@@ -9,6 +9,36 @@ export const mockUser = {
   updatedAt: new Date()
 }
 
+export const user1 = {
+  name: 'Test User1',
+  email: 'Testemail1@crate.com',
+  password: '123',
+  role: params.user.roles.user,
+  createdAt: new Date(),
+  updatedAt: new Date()
+  }
+
+export const user2 = {
+  name: 'Test User2',
+  email: 'Testemail2@crate.com',
+  password: '123',
+  gender: 1,
+  role: params.user.roles.user,
+  createdAt: new Date(),
+  updatedAt: new Date()
+  }
+
+export const user3 = {
+  name: 'Test User3',
+  email: 'Testemail3@test.com',
+  password: '123',
+  role: params.user.roles.user,
+  style: 'casual',
+  gender: 1,
+  createdAt: new Date(),
+  updatedAt: new Date()
+  }
+
 export const mockProduct = {
   name: 'Belt for Women',
   slug: 'belt-for-women',
